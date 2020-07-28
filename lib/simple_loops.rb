4b1971dd725ea6b370f542c57207529a1b9e8212
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 13:19:54 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,9 @@
 # Write your methods here
 
-def loop_message_five_times 
+def loop_message_five_times(str)
+  str = "Loop message five times"
+  counter = 0
 
+while str[counter] do
+puts message
 
