4928d6c3c6dc6303ff781ede9acfd6cebe1f528f
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-28 13:31:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,19 @@
 # Write your methods here
 
-def method_name(parameter)
+#def method_name(parameter)
+ #count = 0
+  #while count < (something)
+    # do something with the parameter
+    #count += 1
+  #end
+#end
+
+def loop_message_five_times(message)
   count = 0
-  while count < (something)
-  #
+  while count < 5 do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, limit)
